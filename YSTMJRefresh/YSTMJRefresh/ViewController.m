802d622235e3b9dcce3089ae63df194063c693a0
styang <812354484@qq.com>
2018-10-12 13:55:52 +0800

    添加刷新

--- YSTMJRefresh/YSTMJRefresh/ViewController.m
+++ YSTMJRefresh/YSTMJRefresh/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#import <MJRefresh/MJRefresh.h>
+#import "YSRefreshGifHeader.h"
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+/** tableView */
+@property (nonatomic, strong) UITableView *tableView;
+/** <#title#> */
 
 @end
 
@@ -16,7 +20,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.frame];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    [self.view addSubview:self.tableView];
+    
+    MJRefreshGifHeader *header =[YSRefreshGifHeader headerWithRefreshingTarget:self refreshingAction:@selector(loadNewData)];
+    self.tableView.mj_header = header;
+}
+
+- (void)loadNewData{
+    sleep(1.0);
+    [self.tableView.mj_header endRefreshing];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *cellID = @"cellID";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"第%ld行", indexPath.row];
+    return cell;
 }
 
 
